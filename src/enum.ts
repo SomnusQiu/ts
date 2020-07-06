ed4c45a3e5c74127f95874f2ee2df61e3d9b84e7
qiu <595628429@qq.com>
2020-07-06 23:38:55 +0800

    对象接口类型

--- src/enum.ts
+++ src/enum.ts
@@ -1,6 +1,6 @@
 //数字枚举
-enum Role{
-    Reporter =1,
+enum Role {
+    Reporter = 1,
     Developer,
     Maintainer,
     Owner,
@@ -27,3 +27,61 @@ enum Role{
 
 
 //常量枚举
+
+//接口返回判断
+interface List {
+    readonly id: number;
+    name: string;
+    //[x:string]:any:
+    age?: number;//可有可无
+}
+interface Result {
+    data: List[]
+}
+
+function render(result: Result) {
+    result.data.forEach(value => {
+        if (value.age) {
+            //需要增加可选属性
+            console.log(value.age)
+        }
+    })
+}
+let result = {
+    data: [{ id: 1, name: 'A', sex: 'male' },
+    { id: 2, name: 'B' }]
+}
+//传入对象只要满足条件 哪怕有多余字段 也可以满足
+
+render(result)
+//如果直接传入的是整个对象
+
+render({
+    data: [{ id: 1, name: 'A', sex: 'male' },
+    { id: 2, name: 'B' }]
+} as Result)
+
+//render(<Result{
+//     data:[{id:1,name:'A',sex:'male'},
+//     {id:2,name:'B'}]
+// })
+//也可以实现 但是在react 中会引起歧义
+
+//第三种字符串索引签名
+//【x:string】:any
+
+
+
+
+interface Stringarray{
+    [index:number] :string
+}
+
+let chars: Stringarray = ['a','b']
+
+interface names {
+    [x:string]:string | number;
+    // y:number
+    [z:number]:number;
+    //数字类型的索引必须是字符串索引的子类型。javascript 比较的时候会把数字转化为string
+}
